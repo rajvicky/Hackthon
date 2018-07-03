90b3113220dff3ee442f91dcd93d8ed895de7a54
UndisputedMaster <Theundisputedmaster2k17@gmail.com>
2018-07-04 01:02:34 +0530

    2018070400

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-sign-up',
@@ -7,9 +9,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpComponent implements OnInit {
 
-  constructor() { }
+  user = { };
+
+  constructor(private http: HttpClient, private router: Router) { }
 
   ngOnInit() {
   }
-
 }
